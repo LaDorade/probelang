e03cc68ebf497e7b6857088ee591ff78b4c83f4e
Mathis <mathisjung02@gmail.com>
2026-04-21 22:25:34 +0200

    initial commit

---
+++ areno.h
@@ -0,0 +1,106 @@
+/**
+ *
+ * Maty Ju, github.com/LaDorade
+ * github.com/LaDorade/my_c_stdlib
+ *
+ * file raw: https://raw.githubusercontent.com/LaDorade/my_c_stdlib/refs/heads/main/areno.h
+ * 
+ */
+
+#ifndef __ARENO_H_
+#define __ARENO_H_
+
+#include <stdlib.h>
+
+#ifndef  ARENO_ASSERT
+#include <assert.h>
+#define  ARENO_ASSERT assert
+#endif //ARENO_ASSERT
+
+#ifndef  ARENO_MALLOC
+#define  ARENO_MALLOC malloc
+#endif //ARENO_MALLOC
+
+#ifndef  ARENO_FREE
+#define  ARENO_FREE free
+#endif //ARENO_FREE
+
+#ifndef  ARENO_CAPACITY
+#define  ARENO_CAPACITY 1024*1024 // 1M
+#endif //ARENO_CAPACITY
+
+typedef struct Areno Areno;
+typedef struct Areno {
+	void*  start;
+	Areno* next;
+	size_t count;
+} Areno;
+
+void *areno_alloc(Areno* areno, size_t size_in_byte);
+void areno_free(Areno* areno);
+void areno_reset(Areno* areno);
+
+#endif // __ARENO_H_
+
+#ifdef ARENO_IMPLEMENTATION
+
+void *areno_alloc(Areno* areno, size_t size_in_byte)
+{
+	ARENO_ASSERT(size_in_byte < ARENO_CAPACITY && "Requested more than one Areno capcity");
+
+	if (areno->start == NULL) { // initial alloc
+		areno->start = ARENO_MALLOC(ARENO_CAPACITY);
+		ARENO_ASSERT(areno->start != NULL);
+	}
+	
+	size_t alignment = ((areno->count + 15) & ~15) - areno->count;
+
+	if (areno->count + alignment + size_in_byte >= ARENO_CAPACITY) { // not enough place in this areno
+		if (areno->next == NULL) {
+			areno->next = (Areno *)ARENO_MALLOC(sizeof(Areno));
+			ARENO_ASSERT(areno->next != NULL);
+
+			*areno->next = (Areno) {0};
+		}
+		return areno_alloc(areno->next, size_in_byte);
+	}
+
+	areno->count += alignment;
+	void *alloc = (char*)areno->start + areno->count;
+	areno->count += size_in_byte;
+
+	return alloc;
+}
+
+void areno_free(Areno* areno)
+{
+	if (areno->start != NULL) {
+		ARENO_FREE(areno->start);
+		areno->start = NULL;
+	}
+
+	Areno *current = areno->next;
+	while (current != NULL) {
+		Areno *to_free = current;
+		current = current->next;
+
+		if (to_free->start != NULL) {
+			ARENO_FREE(to_free->start);
+		}
+		ARENO_FREE(to_free);
+	}
+
+	areno->next  = NULL;
+	areno->count = 0;
+}
+
+void areno_reset(Areno* areno)
+{
+	areno->count = 0;
+	for (Areno* ar = ar->next; ar != NULL; ar = ar->next)
+	{
+		ar->count = 0;
+	}
+}
+
+#endif // ARENO_IMPLEMENTATION
---
+++ main.c
@@ -0,0 +1,327 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string.h>
+#include <unistd.h>
+#include <wctype.h>
+
+#define ARENO_IMPLEMENTATION
+#include "areno.h"
+
+#define BUF_SIZE 1024
+
+typedef struct {
+    char  *items;
+    size_t cursor;
+    size_t count;
+    int    eof;
+} Lexer;
+
+char lex_peek(const Lexer *lex)
+{
+    if    (lex->eof) return 0;
+    return lex->items[lex->cursor];
+}
+
+void lex_advance(Lexer *lex)
+{
+    if (lex->cursor >= lex->count) {
+        lex->eof = 1;
+        return;
+    }
+    lex->cursor += 1;
+}
+
+int lex_match(Lexer *lex, char c)
+{
+    if (lex_peek(lex) == c)
+    {
+        lex_advance(lex);
+        return 1;
+    }
+    return 0;
+}
+
+typedef struct {
+    char  *items;
+    size_t len;
+} String_View;
+
+
+typedef enum {
+    Lex_Invalid = 0,
+
+    // Single char lexeme
+    Lex_Colon,         // :
+    Lex_Comma,         // ,
+    Lex_Semicolon,     // ;
+    Lex_Dot,           // .
+    Lex_Open_bracket,  // (
+    Lex_Close_bracket, // )
+    Lex_Open_brace,    // {
+    Lex_Close_brace,   // }
+    Lex_Plus,          // +
+    Lex_Minus,         // -
+    Lex_Mul,           // *
+    Lex_Divide,        // /
+    Lex_Modulo,        // %
+    Lex_Bang,          // !
+    Lex_Question,      // !
+    Lex_Equal,         // =
+    Lex_Lower,         // <
+    Lex_Greater,       // >
+
+    // Double char lexeme
+    Lex_Colon_Colon,   // ::
+    Lex_Lower_Equal,   // <=
+    Lex_Greater_Equal, // >=
+    Lex_Equal_Equal,   // ==
+    Lex_Not_Equal,     // !=
+
+    // KEYWORDS
+    Lex_local,
+    Lex_let,
+    Lex_return,
+    Lex_reject,
+    Lex_catch,
+    Lex_LastKeyword = Lex_catch,
+
+    // Multi
+    Lex_Ident,
+    Lex_Number,
+    Lex_String,
+
+    Lex_EOF,
+    Lex_LastLex = Lex_EOF
+} Lexeme;
+
+typedef struct {
+    Lexeme kind;
+    union {
+        String_View string;
+        int number;
+    };
+} Token;
+
+int main(void)
+{
+    char *path = "probe.ms";
+    FILE *file = fopen(path, "r");
+    if (!file) {
+        printf("Could not open file at path: %s\n", path);
+        return 1;
+    }
+
+    char buf[BUF_SIZE];
+    memset(buf, 0, sizeof(buf));
+    unsigned long n = fread(buf, 1, BUF_SIZE, file);
+    if (ferror(file)) {
+        printf("Error while reading file at path: %s\n", path);
+        return 1;
+    }
+
+    Lexer lexer = (Lexer) {
+        .items  = buf,
+        .cursor = 0,
+        .count  = strlen(buf),
+    };
+
+    Areno areno = {0};
+    size_t current_tok = 0;
+    Token tokens[BUF_SIZE];
+    memset(tokens, 0, sizeof(tokens));
+    while(!lexer.eof)
+    {
+        char c = lex_peek(&lexer);
+        lex_advance(&lexer);
+        if (lexer.eof) { // end
+            tokens[current_tok++] = (Token) { .kind = Lex_EOF, };
+            break;
+        }
+
+        switch (c) {
+            // double char Lexemes with single char alt
+            case ':':
+                if (lex_match(&lexer, ':'))
+                    tokens[current_tok++] = (Token) { .kind = Lex_Colon_Colon, };
+                else
+                    tokens[current_tok++] = (Token) { .kind = Lex_Colon, };
+                continue;
+            case '!':
+                if (lex_match(&lexer, '='))
+                    tokens[current_tok++] = (Token) { .kind = Lex_Not_Equal, };
+                else
+                    tokens[current_tok++] = (Token) { .kind = Lex_Bang, };
+                continue;
+            case '=':
+                if (lex_match(&lexer, '='))
+                    tokens[current_tok++] = (Token) { .kind = Lex_Equal_Equal, };
+                else
+                    tokens[current_tok++] = (Token) { .kind = Lex_Equal, };
+                continue;
+            case '<':
+                if (lex_match(&lexer, '='))
+                    tokens[current_tok++] = (Token) { .kind = Lex_Lower_Equal, };
+                else
+                    tokens[current_tok++] = (Token) { .kind = Lex_Lower, };
+                continue;
+            case '>':
+                if (lex_match(&lexer, '='))
+                    tokens[current_tok++] = (Token) { .kind = Lex_Greater_Equal, };
+                else
+                    tokens[current_tok++] = (Token) { .kind = Lex_Greater, };
+                continue;
+
+            // single char Lexeme
+            case ',':
+                tokens[current_tok++] = (Token) {.kind = Lex_Comma};
+                continue;
+            case '?':
+                tokens[current_tok++] = (Token) {.kind = Lex_Question};
+                continue;
+            case '.':
+                tokens[current_tok++] = (Token) {.kind = Lex_Dot};
+                continue;
+            case ';':
+                tokens[current_tok++] = (Token) {.kind = Lex_Semicolon};
+                continue;
+            case '(':
+                tokens[current_tok++] = (Token) {.kind = Lex_Open_bracket};
+                continue;
+            case ')':
+                tokens[current_tok++] = (Token) {.kind = Lex_Close_bracket};
+                continue;
+            case '{':
+                tokens[current_tok++] = (Token) {.kind = Lex_Open_brace};
+                continue;
+            case '}':
+                tokens[current_tok++] = (Token) {.kind = Lex_Close_brace};
+                continue;
+            case '+':
+                tokens[current_tok++] = (Token) {.kind = Lex_Plus};
+                continue;
+            case '-':
+                tokens[current_tok++] = (Token) {.kind = Lex_Minus};
+                continue;
+            case '*':
+                tokens[current_tok++] = (Token) {.kind = Lex_Mul};
+                continue;
+            case '/':
+                tokens[current_tok++] = (Token) {.kind = Lex_Divide};
+                continue;
+            case '%':
+                tokens[current_tok++] = (Token) {.kind = Lex_Modulo};
+                continue;
+            case '"': {
+                size_t len = 0;
+                char wordBuf[BUF_SIZE];
+                memset(wordBuf, 0, sizeof(wordBuf));
+
+                while ((c = lex_peek(&lexer)) != '"') {
+                    wordBuf[len++] = c;
+                    lex_advance(&lexer);
+                }
+                lex_advance(&lexer);
+
+                char* items = areno_alloc(&areno, len);
+                strcpy(items, wordBuf);
+                tokens[current_tok++] = (Token) {
+                    .kind = Lex_String,
+                    .string = (String_View) {
+                        .items = items,
+                        .len = len,
+                    }
+                };
+                continue;
+            }
+            default:
+                break;
+        }
+
+        if (isnumber(c))
+        {
+            size_t len = 0;
+            char numBuf[BUF_SIZE];
+            memset(numBuf, 0, sizeof(numBuf));
+            numBuf[len++] = c;
+
+            while (isnumber((c = lex_peek(&lexer))))
+            {
+                lex_advance(&lexer);
+                numBuf[len++] = c;
+            }
+
+            tokens[current_tok++] = (Token) {
+                .kind = Lex_Number,
+                .number = (int)strtol(numBuf, NULL, 10),
+            };
+            if (errno != 0) {
+                printf("Conversion from '%s' to number failed\n", numBuf);
+                exit(1);
+            }
+            continue;
+
+        } else if (isalpha(c))
+        { // ident OR keyword
+            size_t len = 0;
+            char wordBuf[BUF_SIZE];
+            memset(wordBuf, 0, sizeof(wordBuf));
+            wordBuf[len++] = c;
+
+            while (isalnum((c = lex_peek(&lexer))))
+            {
+                lex_advance(&lexer);
+                wordBuf[len++] = c;
+            }
+
+            if (strcmp(wordBuf, "return") == 0)
+                tokens[current_tok++] = (Token) {.kind = Lex_return};
+            else if (strcmp(wordBuf, "reject") == 0)
+                tokens[current_tok++] = (Token) {.kind = Lex_reject};
+            else if (strcmp(wordBuf, "let")    == 0)
+                tokens[current_tok++] = (Token) {.kind = Lex_let};
+            else if (strcmp(wordBuf, "catch")  == 0)
+                tokens[current_tok++] = (Token) {.kind = Lex_catch};
+            else if (strcmp(wordBuf, "local")  == 0)
+                tokens[current_tok++] = (Token) {.kind = Lex_local};
+            else {
+                char* items = areno_alloc(&areno, len);
+                strcpy(items, wordBuf);
+                tokens[current_tok++] = (Token) {
+                    .kind = Lex_Ident,
+                    .string = (String_View) {
+                        .items = items,
+                        .len = len,
+                    }
+                };
+            }
+            continue;
+
+        } else if (isspace(c))
+        {
+            continue;
+
+        } else
+        {
+            printf("Unknown token: %c\n", c);
+        }
+    }
+
+    areno_free(&areno);
+
+    for (int i = 0; i < BUF_SIZE; i++) {
+        Token tok = tokens[i];
+        if (tok.kind == Lex_Ident)
+            printf("Iden: %*s\n", (int)tok.string.len, tok.string.items);
+        else if (tok.kind == Lex_Number)
+            printf("Number: %d\n", tok.number);
+        else if (tok.kind == Lex_String)
+            printf("String: %*s\n", (int)tok.string.len, tok.string.items);
+        else if (tok.kind == Lex_EOF) break;
+    }
+
+    return 0;
+}
