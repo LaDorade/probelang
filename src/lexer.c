eb10642f92f0355214c98dacabef2ead40fb645c
Mathis <mathisjung02@gmail.com>
2026-06-28 11:09:19 +0200

    refacto to use X macros

--- src/lexer.c
+++ src/lexer.c
@@ -1,6 +1,5 @@
 #include "lexer.h"
 
-#include "areno.h"
 #include <assert.h>
 #include <ctype.h>
 #include <errno.h>
@@ -8,8 +7,10 @@
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+#include "areno.h"
 
 #define BUF_SIZE 1024
 
@@ -21,7 +22,7 @@ Token* lexer_lex(Lexer *lexer)
 
     // Reset lexer to the start
     lexer->cursor = 0;
-    lexer->col    = 0;
+    lexer->col    = 1;
     lexer->row    = 1;
 
     while(!lexer->eof)
@@ -35,121 +36,83 @@ Token* lexer_lex(Lexer *lexer)
             break;
         }
 
+        Lexeme kind = Lex_Invalid;
         switch (c) {
-            Lexeme kind = Lex_Invalid;
-            // tripe char Lexemes with double or simple char alt
             case '.':
                 kind = lexer_match(lexer, '.')
                     ? lexer_match(lexer, '.')
                         ? Lex_Dot_Dot_Dot
                         : Lex_Dot_Dot
                     : Lex_Dot;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
+                break;
 
-            // double char Lexemes with single char alt
             case '/': {
                 if (lexer_match(lexer, '/')) { // comment
                     while (!lexer->eof && !lexer_match(lexer, '\n')) {
                         lexer_advance(lexer);
                     }
+                    continue;
                 } else { // divide
                     kind = Lex_Divide;
-                    tokens[current_tok++] = token_create(lexer, kind);
                 }
-            } continue;
-            case ':':
-                kind = lexer_match(lexer, ':') ? Lex_Colon_Colon : Lex_Colon;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
-            case '!':
-                kind = lexer_match(lexer, '=') ? Lex_Not_Equal : Lex_Bang;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
-            case '=':
-                kind = lexer_match(lexer, '=') ? Lex_Equal_Equal : Lex_Equal;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
-            case '<':
-                kind = lexer_match(lexer, '=') ? Lex_Lower_Equal : Lex_Lower;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
-            case '>':
-                kind = lexer_match(lexer, '=') ? Lex_Greater_Equal : Lex_Greater;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
-            case '-':
-                kind = lexer_match(lexer, '>') ? Lex_Arrow_Right : Lex_Minus;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
-
-            // single char Lexeme
-            case ',':
-                kind = Lex_Comma;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
-            case '?':
-                kind = Lex_Question;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
-            case ';':
-                kind = Lex_Semicolon;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
-            case '(':
-                kind = Lex_Open_Paren;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
-            case ')':
-                kind = Lex_Close_Paren;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
-            case '{':
-                kind = Lex_Open_Curly;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
-            case '}':
-                kind = Lex_Close_Curly;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
-            case '+':
-                kind = Lex_Plus;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
-            case '*':
-                kind = Lex_Mul;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
-            case '%':
-                kind = Lex_Modulo;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
-            case '[':
-                kind = Lex_Modulo;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
-            case ']':
-                kind = Lex_Modulo;
-                tokens[current_tok++] = token_create(lexer, kind);
-                continue;
-            case '"':
+            }
+            case '"': // string lit
                 tokens[current_tok++] = lex_string(lexer);
                 continue;
-            default:
+
+            case ':':
+                kind = lexer_match(lexer, ':') ? Lex_Colon_Colon : Lex_Colon;
                 break;
+            case '!':
+                kind = lexer_match(lexer, '=') ? Lex_Not_Equal : Lex_Bang;
+                break;
+            case '=':
+                kind = lexer_match(lexer, '=') ? Lex_Equal_Equal : Lex_Equal;
+                break;
+            case '<':
+                kind = lexer_match(lexer, '=') ? Lex_Lower_Equal : Lex_Lower;
+                break;
+            case '>':
+                kind = lexer_match(lexer, '=') ? Lex_Greater_Equal : Lex_Greater;
+                break;
+            case '-':
+                kind = lexer_match(lexer, '>') ? Lex_Arrow_Right : Lex_Minus;
+                break;
+
+                // single char Lexeme
+            #define X(c, _, lexeme) case c: kind = lexeme; break;
+                SINGLE_CHARS
+            #undef X
+
+        }
+        if (kind != Lex_Invalid) {
+            tokens[current_tok++] = token_create(lexer, kind);
+            continue;
         }
 
         if (isdigit(c)) {
             tokens[current_tok++] = lex_digit(lexer);
-        } else if (isalpha(c)) { // ident OR keyword
+        } else if (isalpha(c) || c == '_') { // ident OR keyword
             tokens[current_tok++] = lex_ident(lexer);
         } else if (isspace(c)) {
             continue;
         } else {
-            // TODO: lexer error handling
-            printf("Unknown token: %c at %ld:%ld\n", c, lexer->row, lexer->col);
+            Token guilty = token_create(lexer, Lex_Invalid);
+            guilty.as.chr = c;
+            Lex_Error err = (Lex_Error) {
+                .guilty = guilty,
+                .formatted = areno_printf(lexer->areno, "Unrecognized char '%c'\n", c),
+            };
+            areno_arr_push(lexer->areno, &lexer->err, err);
         }
     }
+
+    // maybe remove ?
+    // we sure know that we errored, but lexer
+    // is in a valid state
+    // But, we dont want to continue (sure?)
+    if (lexer->err.count) return NULL; 
+
     return tokens;
 }
 
@@ -194,8 +157,12 @@ Token lex_digit(Lexer *lexer)
     Token tok = token_create(lexer, Lex_Number);
     tok.as.number = strtod(numBuf, NULL);
     if (errno != 0) {
-        printf("Conversion from '%s' to double precision float failed\n", numBuf);
-        exit(1);
+        tok.as.number = 0;
+        Lex_Error err = (Lex_Error) {
+            .guilty = tok,
+            .formatted = areno_printf(lexer->areno, "Conversion from '%s' to double precision float failed\n", numBuf),
+        };
+        areno_arr_push(lexer->areno, &lexer->err, err);
     }
     return tok;
 }
@@ -208,68 +175,21 @@ Token lex_ident(Lexer *lexer)
     memset(wordBuf, 0, sizeof(wordBuf));
     wordBuf[len++] = c;
 
-    while (!lexer->eof && isalnum((c = lexer_peek(lexer))))
+    while (!lexer->eof && (isalnum((c = lexer_peek(lexer))) || c == '_'))
     {
         lexer_advance(lexer);
         wordBuf[len++] = c;
     }
 
     Token tok = token_create(lexer, Lex_Invalid);
-    if      (strcmp(wordBuf, "struct") == 0)
-        tok.kind = Lex_struct;
-    else if (strcmp(wordBuf, "union")  == 0)
-        tok.kind = Lex_union;
-    else if (strcmp(wordBuf, "enum")   == 0)
-        tok.kind = Lex_enum;
-    else if (strcmp(wordBuf, "type")   == 0)
-        tok.kind = Lex_type;
-    else if (strcmp(wordBuf, "module") == 0)
-        tok.kind = Lex_module;
-    else if (strcmp(wordBuf, "use")    == 0)
-        tok.kind = Lex_use;
-    else if (strcmp(wordBuf, "using")  == 0)
-        tok.kind = Lex_using;
-    else if (strcmp(wordBuf, "fun")    == 0)
-        tok.kind = Lex_fun;
-    else if (strcmp(wordBuf, "let")    == 0)
-        tok.kind = Lex_let;
-    else if (strcmp(wordBuf, "const")  == 0)
-        tok.kind = Lex_const;
-    else if (strcmp(wordBuf, "defer")  == 0)
-        tok.kind = Lex_defer;
-    else if (strcmp(wordBuf, "return") == 0)
-        tok.kind = Lex_return;
-    else if (strcmp(wordBuf, "reject") == 0)
-        tok.kind = Lex_reject;
-    else if (strcmp(wordBuf, "local")  == 0)
-        tok.kind = Lex_local;
-    else if (strcmp(wordBuf, "if")     == 0)
-        tok.kind = Lex_if;
-    else if (strcmp(wordBuf, "else")   == 0)
-        tok.kind = Lex_else;
-    else if (strcmp(wordBuf, "switch") == 0)
-        tok.kind = Lex_switch;
-    else if (strcmp(wordBuf, "match")  == 0)
-        tok.kind = Lex_match;
-    else if (strcmp(wordBuf, "do")     == 0)
-        tok.kind = Lex_do;
-    else if (strcmp(wordBuf, "while")  == 0)
-        tok.kind = Lex_while;
-    else if (strcmp(wordBuf, "for")    == 0)
-        tok.kind = Lex_for;
-    else if (strcmp(wordBuf, "break")  == 0)
-        tok.kind = Lex_break;
-    else if (strcmp(wordBuf, "continue")  == 0)
-        tok.kind = Lex_continue;
-    else if (strcmp(wordBuf, "or")  == 0)
-        tok.kind = Lex_or;
-    else if (strcmp(wordBuf, "and")  == 0)
-        tok.kind = Lex_and;
-    else if (strcmp(wordBuf, "try")    == 0)
-        tok.kind = Lex_try;
-    else if (strcmp(wordBuf, "catch")  == 0)
-        tok.kind = Lex_catch;
-    else {
+    for (size_t i = 0; i < keyword_nb; i++) {
+        if (strcmp(wordBuf, keywords[i]) == 0) {
+            tok.kind = keyword_lexemes[i];
+            break;
+        }
+    }
+
+    if (tok.kind == Lex_Invalid) { // not a keyword
         char* items = areno_alloc(lexer->areno, len);
         strcpy(items, wordBuf);
 
@@ -296,7 +216,7 @@ static inline void lexer_advance(Lexer *lex)
 {
     if (lexer_peek(lex) == '\n') {
         lex->row += 1;
-        lex->col  = 0;
+        lex->col  = 0; // \n count as the "first" (0 indexed) char
     } else {
         lex->col += 1;
     }
@@ -327,11 +247,13 @@ static inline bool lexer_match(Lexer *lex, char c)
 
 static inline Token token_create(const Lexer* lex, Lexeme lexeme)
 {
+    // is always minimum at 1 except for EOF or INVALID
+    size_t size = lex->cursor - lex->start;
     return (Token) {
         .kind = lexeme,
-        .col  = lex->col,
+        .col  = lex->col - size + 1,
         .row  = lex->row,
-        .size = lex->cursor - lex->start,
+        .size = size,
     };
 }
 
@@ -341,28 +263,19 @@ const char* lexer_print(Lexeme lexeme)
 {
     switch (lexeme) {
         // Single char lexeme
-        case Lex_Colon:        return ":";
-        case Lex_Comma:        return ",";
-        case Lex_Semicolon:    return ";";
-        case Lex_Dot:          return ".";
-        case Lex_Open_Paren:   return "(";
-        case Lex_Close_Paren:  return ")";
-        case Lex_Open_Curly:   return "{";
-        case Lex_Close_Curly:  return "}";
-        case Lex_Open_Square:  return "[";
-        case Lex_Close_Square: return "]";
-        case Lex_Plus:         return "+";
-        case Lex_Minus:        return "-";
-        case Lex_Mul:          return "*";
-        case Lex_Divide:       return "/";
-        case Lex_Modulo:       return "%";
-        case Lex_Bang:         return "!";
-        case Lex_Question:     return "?";
-        case Lex_Equal:        return "=";
-        case Lex_Lower:        return "<";
-        case Lex_Greater:      return ">";
+#define X(_, str, lexeme) case lexeme: return str;
+    SINGLE_CHARS
+#undef X
 
         // Double char lexeme
+        case Lex_Colon:         return ":";
+        case Lex_Dot:           return ".";
+        case Lex_Divide:        return "/";
+        case Lex_Bang:          return "!";
+        case Lex_Equal:         return "=";
+        case Lex_Lower:         return "<";
+        case Lex_Greater:       return ">";
+        case Lex_Minus:         return "-";
         case Lex_Dot_Dot:       return "..";
         case Lex_Colon_Colon:   return "::";
         case Lex_Lower_Equal:   return "<=";
@@ -375,50 +288,16 @@ const char* lexer_print(Lexeme lexeme)
         case Lex_Dot_Dot_Dot:   return "...";
 
         // KEYWORDS
-        case Lex_struct:    return "struct";
-        case Lex_union:     return "union";
-        case Lex_enum:      return "enum";
-        case Lex_type:      return "type";
-        case Lex_interface: return "interface";
+#define X(name, lexeme) case lexeme: return #name;
+    KEYWORDS
+#undef X
 
-        case Lex_module:    return "module";
-        case Lex_use:       return "use";
-        case Lex_using:     return "using";
-
-        case Lex_extern: return "extern";
-        case Lex_static: return "static";
-        case Lex_let:    return "let";
-        case Lex_const:  return "const";
-
-        case Lex_fun:  return "fun";
-
-        case Lex_defer:  return "defer";
-        case Lex_return: return "return";
-        case Lex_reject: return "reject";
-        case Lex_local:  return "local";
-
-        case Lex_if:     return "if";
-        case Lex_else:   return "else";
-        case Lex_match:  return "match";
-        case Lex_switch: return "switch";
-        case Lex_break:  return "break";
-        case Lex_or:     return "or";
-        case Lex_and:    return "and";
-
-        case Lex_do:     return "do";
-        case Lex_while:  return "while";
-        case Lex_for:    return "for";
-        case Lex_continue: return "continue";
-
-        case Lex_try:    return "try";
-        case Lex_catch:  return "catch";
-
-        case Lex_Ident:  return "IDENT";
-        case Lex_Number: return "NUMBER";
+        case Lex_Ident:      return "IDENT";
+        case Lex_Number:     return "NUMBER";
         case Lex_String_Lit: return "STRING";
 
-        case Lex_EOF:     return "EOF";
-        case Lex_Invalid: return "INVALID LEXEME";
+        case Lex_EOF:        return "EOF";
+        case Lex_Invalid:    return "INVALID LEXEME";
     }
     assert(0 && "UNREACHABLE");
     return "UNREACHABLE";
--- src/lexer.h
+++ src/lexer.h
@@ -3,96 +3,104 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+
 #include "areno.h"
 #include "string_view.h"
 
-typedef struct {
-    String_View sv;
-    Areno      *areno;
-    size_t      cursor;
-    // save the indice of start of current Token
-    size_t      start;
-    size_t      col, row;
-    bool        eof;
-} Lexer;
+#define SINGLE_CHARS \
+    X(',', ",", Lex_Comma)        \
+    X(';', ";", Lex_Semicolon)    \
+    X('(', "(", Lex_Open_Paren)   \
+    X(')', ")", Lex_Close_Paren)  \
+    X('{', "{", Lex_Open_Curly)   \
+    X('}', "}", Lex_Close_Curly)  \
+    X('[', "[", Lex_Open_Square)  \
+    X(']', "]", Lex_Close_Square) \
+    X('+', "+", Lex_Plus)         \
+    X('*', "*", Lex_Mul)          \
+    X('%', "%", Lex_Modulo)       \
+    X('?', "?", Lex_Question)     \
+
+
+#define KEYWORDS \
+    X(struct,Lex_struct) \
+    X(union,Lex_union) \
+    X(enum,Lex_enum) \
+    X(type,Lex_type) \
+    X(interface,Lex_interface) \
+\
+    X(module,Lex_module) \
+    X(use,Lex_use) \
+    X(using,Lex_using) \
+\
+    X(fun,Lex_fun) \
+\
+    X(extern,Lex_extern) \
+    X(static,Lex_static) \
+    X(let,Lex_let) \
+    X(const,Lex_const) \
+\
+    X(defer,Lex_defer) \
+    X(return,Lex_return) \
+    X(reject,Lex_reject) \
+    X(local,Lex_local) \
+\
+    X(if,Lex_if) \
+    X(else,Lex_else) \
+    X(match,Lex_match) \
+    X(switch,Lex_switch) \
+    X(break,Lex_break) \
+    X(or,Lex_or) \
+    X(and,Lex_and) \
+\
+    X(do,Lex_do) \
+    X(while,Lex_while) \
+    X(for,Lex_for) \
+    X(continue,Lex_continue) \
+\
+    X(try,Lex_try) \
+    X(catch,Lex_catch)
+
+static const char* keywords[] = {
+#define X(name, _) #name,
+    KEYWORDS
+#undef X
+};
+
+static const int keyword_nb = sizeof(keywords) / sizeof(keywords[0]);
 
 typedef enum {
     Lex_Invalid = 0,
 
     // Single char lexeme
+#define X(_, __, lexeme) lexeme,
+    SINGLE_CHARS
+#undef X
+
+    // Double char lexeme
     Lex_Colon,         // :
-    Lex_Comma,         // ,
-    Lex_Semicolon,     // ;
-    Lex_Dot,           // .
-    Lex_Open_Paren,    // (
-    Lex_Close_Paren,   // )
-    Lex_Open_Curly,    // {
-    Lex_Close_Curly,   // }
-    Lex_Open_Square,   // [
-    Lex_Close_Square,  // ]
-    Lex_Plus,          // +
-    Lex_Minus,         // -
-    Lex_Mul,           // *
+    Lex_Colon_Colon,   // ::
     Lex_Divide,        // /
-    Lex_Modulo,        // %
     Lex_Bang,          // !
-    Lex_Question,      // ?
     Lex_Equal,         // =
     Lex_Lower,         // <
     Lex_Greater,       // >
-
-    // Double char lexeme
-    Lex_Dot_Dot,       // ..
-    Lex_Colon_Colon,   // ::
     Lex_Lower_Equal,   // <=
     Lex_Greater_Equal, // >=
     Lex_Equal_Equal,   // ==
     Lex_Not_Equal,     // !=
+    Lex_Minus,         // -
     Lex_Arrow_Right,   // ->
 
     // Triple char lexeme
     Lex_Dot_Dot_Dot,   // ...
+    Lex_Dot_Dot,       // ..
+    Lex_Dot,           // .
 
     // KEYWORDS
-    Lex_struct,
-    Lex_union,
-    Lex_enum,
-    Lex_type,
-    Lex_interface,
-
-    Lex_module,
-    Lex_use,
-    Lex_using,
-
-    Lex_fun,
-
-    Lex_extern,
-    Lex_static,
-    Lex_let,
-    Lex_const,
-
-    Lex_defer,
-    Lex_return,
-    Lex_reject,
-    Lex_local,
-
-    Lex_if,
-    Lex_else,
-    Lex_match,
-    Lex_switch,
-    Lex_break,
-    Lex_or,
-    Lex_and,
-
-    Lex_do,
-    Lex_while,
-    Lex_for,
-    Lex_continue,
-
-    Lex_try,
-    Lex_catch,
-
-    Lex_LastKeyword = Lex_catch,
+#define X(_, lexeme) lexeme,
+    KEYWORDS
+#undef X
 
     // Multi
     Lex_Ident,
@@ -103,18 +111,48 @@ typedef enum {
     Lex_LastLex = Lex_EOF
 } Lexeme;
 
+static const Lexeme keyword_lexemes[] = {
+#define X(_, lexeme) lexeme,
+    KEYWORDS
+#undef X
+};
+
 typedef struct {
     union {
+        char        chr;
         double      number;
         String_View string;
         String_View ident;
     } as;
 
     size_t size; // nb of char of the Token
-    size_t row;
-    size_t col;
+    size_t row; // 1 indexed
+    size_t col; // 1 indexed
     Lexeme kind;
 } Token;
+
+typedef struct {
+    Token  guilty;
+    char  *formatted;
+} Lex_Error;
+
+typedef struct {
+    Lex_Error *items;
+    size_t     count;
+    size_t     capacity;
+} Lex_Errors;
+
+typedef struct {
+    Lex_Errors  err;
+    String_View sv;
+    Areno      *areno;
+    size_t      cursor;
+    // save the indice of start of current Token
+    size_t start;
+    size_t col, row;
+    bool   eof;
+} Lexer;
+
 
 Token* lexer_lex (Lexer* lexer);
 Token  lex_string(Lexer* lexer);
